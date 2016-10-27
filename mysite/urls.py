bd80e03953086160c57430aa6f2606df4644dbf9
cmchecha <cmchechaa@gmail.com>
2016-10-27 00:07:29 -0600

    PRIMER TEMPLATE LISTADO DE PRODUCTOS

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('ventas.urls')),
 ]
--- ventas/views.py
+++ ventas/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def listado_productos(request):
+        return render(request, 'ventas/listado_productos.html', {})
